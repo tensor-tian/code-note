12a21d1613694a60bb518df9e1ebe2700dc65bb9
tensor-tian <tensor.tian@gmail.com>
2024-07-05 00:20:54 +0800

    fix code block style

--- pkgs/web/src/utils.ts
+++ pkgs/web/src/utils.ts
@@ -85,66 +85,12 @@ protected _register<T extends vscode.Disposable>(value: T): T {
   text: `#### \`disposeAll\` dispose by hand 5 
   some text <Reference to="255s">_\`test\`_</Reference> Emphasis, aka italics, with *asterisks* or _underscores_.
 
-# H1
-## H2
-### H3
-#### H4
-##### H5
-###### H6
-
-Emphasis, aka italics, with *asterisks* or _underscores_.
-
-Strong emphasis, aka bold, with **asterisks** or __underscores__.
-
-Combined emphasis with **asterisks and _underscores_**.
-
-Strikethrough uses two tildes. ~~Scratch this.~~
-
-1. First ordered list item
-2. Another item
-  - Unordered sub-list.
-1. Actual numbers don't matter, just that it's a number
-  1. Ordered sub-list
-4. And another item.
-
-
-
-- Unordered list can use asterisks
-- Or minuses
-- Or pluses
-
-1. foo
-  1. bar
-    1. baz
-      a. faz
-2. foo2
-
-- foo
-  - bar
-    - baz
-      - faz
-- foo2
-
-[I'm an inline-style link](https://www.google.com)
-
-
-Inline-style:
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
-
-Inline \`code\` has \`back-ticks around\` it.
-
 | Tables        | Are           | Cool  |
 | ------------- |:-------------:| -----:|
 | col 3 is      | right-aligned |  |
 | col 2 is      | centered      |    |
 | zebra stripes | are neat      |     |
 
-> Blockquotes are very handy in email to emulate reply text.
-> This line is part of the same quote.
-
-Quote break.
-
-> This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.
 
 `,
   ranges: "[[], [], [], []]",
